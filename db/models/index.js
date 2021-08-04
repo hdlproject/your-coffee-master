dc5851824e18bd3e01cf4ebeb02d1bd0570f3aff
Hendra Danu Laksana <hdl.project.co@gmail.com>
2021-08-04 23:51:06 +0700

    fix(db): move env reader to models/index.js

--- db/models/index.js
+++ db/models/index.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs');
 const path = require('path');
+
+require('dotenv').config({path: path.join(__dirname, '../../.env')});
+
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
